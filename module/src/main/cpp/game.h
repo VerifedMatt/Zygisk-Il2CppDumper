28d0e7273dff7a25e681959bd25ef65cfb2372e0
VerifedMatt <103627962+VerifedMatt@users.noreply.github.com>
2024-12-15 12:32:36 +0100

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.habby.capybara"
+#define GamePackageName "com.dxx.firenow"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
